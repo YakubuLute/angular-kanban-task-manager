c23cbe131ab3f71ccfe232ca6f363ed0a332353c
Yakubu Lute <yakubulute@outlook.com>
2025-04-18 16:39:31 +0000

    Added task card component html implementations

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core'
 import { Task } from '../../models/task.model'
+import { NgFor, NgIf } from '@angular/common'
 
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
-  styleUrls: ['./task-card.component.scss']
+  styleUrls: ['./task-card.component.scss'],
+  imports: [NgFor, NgIf]
 })
 export class TaskCardComponent {
   @Input() task!: Task
