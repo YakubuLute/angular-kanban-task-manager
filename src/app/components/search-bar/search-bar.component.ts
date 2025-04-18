f926a853df709700a142edee264c96e7618ec9a3
Yakubu Lute <yakubulute@outlook.com>
2025-04-18 16:36:27 +0000

    Fixed type issues in seach component

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core'
+import { Component, NgModule } from '@angular/core'
 import { TaskService } from '../../services/task.service'
+import { NgModel, FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
+  imports: [
+    FormsModule,
+  ],
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
