229b7ac1ad4e60c5d7a6ad64f0c4faafb3f4ffd0
Yakubu Lute <yakubulute@outlook.com>
2025-04-18 16:56:41 +0000

    Updated main html and component to render re-usable components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { SearchBarComponent } from "./components/search-bar/search-bar.component";
+import { AddTaskComponent } from "./components/add-task/add-task.component";
+import { TaskBoardComponent } from "./components/task-board/task-board.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchBarComponent, AddTaskComponent, TaskBoardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Task Management'
 }
